e5b6cd3c60e0ca985b99342ee64a7dc2e4ad37ba
nyleLarsen <nylarseen@gmail.com>
2024-04-12 12:34:54 -0600

    simplePostest

---
+++ test/pageobjects/login.page.js
@@ -0,0 +1,31 @@
+import { $ } from '@wdio/globals'
+import Page from './page.js';
+
+
+class LoginPage extends Page {
+
+    get inputUsername () {
+        return $('#user-name');
+    }
+
+    get inputPassword () {
+        return $('#password');
+    }
+
+    get btnSubmit () {
+        return $('#login-button');
+    }
+
+
+    async login (username, password) {
+        await this.inputUsername.setValue(username);
+        await this.inputPassword.setValue(password);
+        await this.btnSubmit.click();
+    }
+
+    open () {
+        return super.open('login');
+    }
+}
+
+export default new LoginPage();
